074e9b2d3abd1b78303c3fe42133a7c38fae3c63
prabath1998 <prabhathudayanga1998@gmail.com>
2024-02-11 18:25:57 +0530

    implement jwt auth

---
+++ User-Service/src/main/java/com/ontrack/userservice/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.ontrack.userservice.config;
+
+public class JwtConstant {
+    public  static final String SECRET_KEY = "123456789";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ User-Service/src/main/java/com/ontrack/userservice/modal/User.java
@@ -0,0 +1,21 @@
+package com.ontrack.userservice.modal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String password;
+    private String email;
+    private String role;
+    private String fullName;
+}
